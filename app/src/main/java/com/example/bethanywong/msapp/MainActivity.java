edcc4443b995ead9d50ddaa8d512e71a0969bb42
bbwong23 <bethanywong1216@gmail.com>
2017-02-05 17:45:13 -0500

    created additional pages for organization and applied activity navigations on existing pages

---
+++ app/src/main/java/com/example/bethanywong/msapp/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.bethanywong.msapp;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import static android.R.attr.start;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        OnClickButtonListener();
+    }
+
+    public void OnClickButtonListener() {
+        Button test1_button = (Button)findViewById(R.id.tap_button);
+        Button devinfo_button = (Button)findViewById(R.id.devinfo_button);
+        test1_button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("com.example.bethanywong.msapp.test");
+                        startActivity(intent);
+                    }
+                }
+        );
+
+        devinfo_button.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Intent intent = new Intent("com.example.bethanywong.msapp.DeveloperInfo");
+                        startActivity(intent);
+                    }
+                }
+        );
+    }
+
+}
