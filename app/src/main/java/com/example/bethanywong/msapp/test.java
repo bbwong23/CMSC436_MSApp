94af7a2413c76b0737cdec79426089212176dac2
Charles Wang <charlesysw@gmail.com>
2017-02-07 19:42:46 -0500

    added warmup and cooldown timers

--- app/src/main/java/com/example/bethanywong/msapp/test.java
+++ app/src/main/java/com/example/bethanywong/msapp/test.java
@@ -1,14 +1,17 @@
 package com.example.bethanywong.msapp;
 
+import android.graphics.PorterDuff;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.graphics.Color;
 
 public class test extends AppCompatActivity {
     CountDownTimer timer = new CountDownTimer(10000, 1000) {
@@ -17,6 +20,7 @@ public class test extends AppCompatActivity {
         }
 
         public void onFinish() {
+            isCountingDown = false;
             countHistory[round-1] = count;
             count=0;
             round++;
@@ -50,6 +54,7 @@ public class test extends AppCompatActivity {
     String hand;
     private int[] countHistory = new int[4];
     Animation wiggle;
+    boolean isCountingDown;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -60,27 +65,64 @@ public class test extends AppCompatActivity {
         round = 1;
         hand = "right";
         wiggle = AnimationUtils.loadAnimation(this, R.anim.wiggle);
+        isCountingDown = false;
     }
 
     public void runTimer(View view) {
         tap = (ImageView)findViewById(R.id.tapCount);
+        instructions = (TextView) findViewById(R.id.instructions);
+        txtCount = (TextView) findViewById(R.id.tapBegin);
         tap.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 count++;
+                //Log.d("runTimer",String.valueOf(count));
                 v.startAnimation(wiggle);
             }
         });
-        timer.start();
-        instructions = (TextView) findViewById(R.id.instructions);
-        txtCount = (TextView) findViewById(R.id.tapBegin);
+        if (!isCountingDown){
+            tap.setEnabled(false);
+            tap.setColorFilter(Color.rgb(123,123,123), PorterDuff.Mode.MULTIPLY);
+            CountDownTimer warmUp = new CountDownTimer(3000,1000) {
+                @Override
+                public void onTick(long millisUntilFinished) {
+                    if (millisUntilFinished/1000 == 2){
+                        txtCount.setText("Ready!");
+                    } else if (millisUntilFinished/1000 == 1){
+                        txtCount.setText("Set!");
+                    }
+                }
+                @Override
+                public void onFinish() {
+                    tap.clearColorFilter();
+                    txtCount.setText("Tap!");
+                    tap.setEnabled(true);
+                    timer.start();
+                }
+            };
+            timeTextView.setText("10");
+            warmUp.start();
+        }
+        isCountingDown = true;
         count = 0;
-        txtCount.setText("Tap!");
         instructions.setText("Keep your hand in place!");
-
+    }
+    public void coolDown(){
+        tap.setEnabled(false);
+        CountDownTimer coolDown = new CountDownTimer(2000,1000) {
+            @Override
+            public void onTick(long millisUntilFinished) {
+            }
+            @Override
+            public void onFinish() {
+                tap.setEnabled(true);
+            }
+        };
+        coolDown.start();
     }
 
     public void setNewRound() {
+        coolDown();
         timeTextView.setText("TIME'S UP");
         txtCount.setText("Tap to begin!");
         instructions.setText("Place " + hand + " hand on phone with index finger on the corgi!");
@@ -92,6 +134,7 @@ public class test extends AppCompatActivity {
     }
 
     public void displayResults() {
+        coolDown();
         hand = "right";
         round = 1;
         timeTextView.setText("TIME'S UP");
