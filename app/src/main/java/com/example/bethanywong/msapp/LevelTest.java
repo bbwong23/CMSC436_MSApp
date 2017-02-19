163e40641ab55e4023dad7f94e24bc98dcc9f96e
Charles Wang <charlesysw@gmail.com>
2017-02-19 16:54:05 -0500

    first tutorial didn't work, trying second

--- app/src/main/java/com/example/bethanywong/msapp/LevelTest.java
+++ app/src/main/java/com/example/bethanywong/msapp/LevelTest.java
@@ -1,145 +1,116 @@
 package com.example.bethanywong.msapp;
 
-import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import java.util.Timer;
-import java.util.TimerTask;
-import android.app.Activity;
-import android.os.Bundle;
-import android.os.Handler;
-import android.view.Display;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.Window;
-import android.view.WindowManager.LayoutParams;
-import android.widget.FrameLayout;
+import android.content.Context;
+import android.content.pm.ActivityInfo;
+import android.content.res.Configuration;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Point;
+import android.graphics.drawable.ShapeDrawable;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener2;
 import android.hardware.SensorManager;
-import android.content.Context;
-import android.content.res.Configuration;
-import android.hardware.SensorEventListener;
-import android.widget.RelativeLayout;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Display;
+import android.view.View;
+import android.view.Window;
 
-public class LevelTest extends AppCompatActivity {
-    BallView mBallView = null;
-    Handler RedrawHandler = new Handler(); //so redraw occurs in main thread
-    Timer mTmr = null;
-    TimerTask mTsk = null;
-    int mScrWidth, mScrHeight;
-    android.graphics.PointF mBallPos, mBallSpd;
+public class LevelTest extends AppCompatActivity implements  SensorEventListener2{
+    private float xPos, xAccel, xVel = 0.0f;
+    private float yPos, yAccel, yVel = 0.0f;
+    private float xMax, yMax;
+    private Bitmap ball;
+    private SensorManager sensorManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_level_test);
+        setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        BallView ballView = new BallView(this);
+        setContentView(ballView);
 
-        final RelativeLayout mainView = (android.widget.RelativeLayout)findViewById(R.id.activity_level_test);
-        //get screen dimensions
+        Point size = new Point();
         Display display = getWindowManager().getDefaultDisplay();
-        mScrWidth = display.getWidth();
-        mScrHeight = display.getHeight();
-        mBallPos = new android.graphics.PointF();
-        mBallSpd = new android.graphics.PointF();
+        display.getSize(size);
+        xMax = (float) size.x - 100;
+        yMax = (float) size.y - 100;
 
-        //create variables for ball position and speed
-        mBallPos.x = mScrWidth/2;
-        mBallPos.y = mScrHeight/2;
-        mBallSpd.x = 0;
-        mBallSpd.y = 0;
-        //create initial ball
-        mBallView = new BallView(this, mBallPos.x, mBallPos.y, 5);
-
-        mainView.addView(mBallView); //add ball to main screen
-        mBallView.invalidate(); //call onDraw in BallView
-
-        //listener for accelerometer, use anonymous class for simplicity
-        ((SensorManager)getSystemService(Context.SENSOR_SERVICE)).registerListener(
-        new SensorEventListener() {
-            @Override
-            public void onSensorChanged(SensorEvent event) {
-                //set ball speed based on phone tilt (ignore Z axis)
-                mBallSpd.x = -event.values[0];
-                mBallSpd.y = event.values[1];
-                //timer event will redraw ball
-            }
-            @Override
-            public void onAccuracyChanged(Sensor sensor, int accuracy) {} //ignore
-        },
-        ((SensorManager)getSystemService(Context.SENSOR_SERVICE))
-                .getSensorList(Sensor.TYPE_ACCELEROMETER).get(0),
-        SensorManager.SENSOR_DELAY_NORMAL);
-    }
-    //listener for menu button on phone
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        menu.add("Exit"); //only one menu item
-        return super.onCreateOptionsMenu(menu);
-    }
-    //listener for menu item clicked
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        // Handle item selection
-        if (item.getTitle() == "Exit") //user clicked Exit
-            finish(); //will call onPause
-        return super.onOptionsItemSelected(item);
-    }
-    //For state flow see http://developer.android.com/reference/android/app/Activity.html
-    @Override
-    public void onPause() //app moved to background, stop background threads
-    {
-        mTmr.cancel(); //kill\release timer (our only background thread)
-        mTmr = null;
-        mTsk = null;
-        super.onPause();
-    }
-    @Override
-    public void onResume() //app moved to foreground (also occurs at app startup)
-    {
-        //create timer to move ball to new position
-        mTmr = new Timer();
-        mTsk = new TimerTask() {
-            public void run() {
-                //if debugging with external device,
-//  a log cat viewer will be needed on the device
-                android.util.Log.d("TiltBall", "Timer Hit - " + mBallPos.x + ":" + mBallPos.y);
-                //move ball based on current speed
-                mBallPos.x += mBallSpd.x;
-                mBallPos.y += mBallSpd.y;
-                //if ball goes off screen, reposition to opposite side of screen
-                if (mBallPos.x > mScrWidth) mBallPos.x=0;
-                if (mBallPos.y > mScrHeight) mBallPos.y=0;
-                if (mBallPos.x < 0) mBallPos.x=mScrWidth;
-                if (mBallPos.y < 0) mBallPos.y=mScrHeight;
-                //update ball class instance
-
-                //for some reason this doesnt work?
-//                mBallView.mX = mBallPos.x;
-//                mBallView.mY = mBallPos.y;
-                //redraw ball. Must run in background thread to prevent thread lock.
-                RedrawHandler.post(new Runnable() {
-                    public void run() {
-                        mBallView.invalidate();
-                    }});
-            }};
-        super.onResume();
+        sensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
     }
 
     @Override
-    public void onDestroy() //main thread stopped
-    {
-        super.onDestroy();
-        //wait for threads to exit before clearing app
-        System.runFinalizersOnExit(true);
-        //remove app from memory
-        android.os.Process.killProcess(android.os.Process.myPid());
+    protected void onStart() {
+        super.onStart();
+        sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
     }
 
     @Override
-    public void onConfigurationChanged(Configuration newConfig)
-    {
-        super.onConfigurationChanged(newConfig);
+    protected void onStop() {
+        sensorManager.unregisterListener(this);
+        super.onStop();
+    }
+
+    @Override
+    public void onFlushCompleted(Sensor sensor) {
+
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent sensorEvent) {
+        if (sensorEvent.sensor.getType() == Sensor.TYPE_ACCELEROMETER) {
+            xAccel = sensorEvent.values[0];
+            yAccel = -sensorEvent.values[1];
+            updateBall();
+        }
+    }
+
+    private void updateBall() {
+        float frameTime = 0.666f;
+        xVel += (xAccel * frameTime);
+        yVel += (yAccel * frameTime);
+
+        float xS = (xVel / 2) * frameTime;
+        float yS = (yVel / 2) * frameTime;
+
+        xPos -= xS;
+        yPos -= yS;
+
+        if (xPos > xMax) {
+            xPos = xMax;
+        } else if (xPos < 0) {
+            xPos = 0;
+        }
+
+        if (yPos > yMax) {
+            yPos = yMax;
+        } else if (yPos < 0) {
+            yPos = 0;
+        }
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int i) {
+
+    }
+
+    private class BallView extends View {
+
+        public BallView(Context context) {
+            super(context);
+            Bitmap ballSrc = BitmapFactory.decodeResource(getResources(), R.drawable.ball);
+            final int dstWidth = 100;
+            final int dstHeight = 100;
+            ball = Bitmap.createScaledBitmap(ballSrc, dstWidth, dstHeight, true);
+        }
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            canvas.drawBitmap(ball, xPos, yPos, null);
+            invalidate();
+        }
     }
 }
