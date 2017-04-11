1b5ab07f5aca5dbda28b411f0f442c5c7d12d6bf
Charles Wang <charlesysw@gmail.com>
2017-04-11 10:50:47 -0400

    sheets response for sway test

--- app/src/main/java/com/example/bethanywong/msapp/SwayScoreFragment.java
+++ app/src/main/java/com/example/bethanywong/msapp/SwayScoreFragment.java
@@ -2,8 +2,10 @@ package com.example.bethanywong.msapp;
 
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,15 +16,15 @@ import static com.example.bethanywong.msapp.TapTest.RESULTS_KEY;
 
 
 public class SwayScoreFragment extends Fragment {
-    private double[] results;
+    private float[] results;
     private TextView resultsText;
     private Button homeButton;
     private OnFinishSwayTestListener callBack;
 
-    public static SwayScoreFragment newInstance(double[] results) {
+    public static SwayScoreFragment newInstance(float[] results) {
         SwayScoreFragment fragment = new SwayScoreFragment();
         Bundle args = new Bundle();
-        args.putDoubleArray(RESULTS_KEY, results);
+        args.putFloatArray(RESULTS_KEY, results);
         fragment.setArguments(args);
         return fragment;
     }
@@ -36,13 +38,14 @@ public class SwayScoreFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_sway_score, container, false);
-        results = getArguments().getDoubleArray(RESULTS_KEY);
+        results = getArguments().getFloatArray(RESULTS_KEY);
         resultsText = (TextView)view.findViewById(R.id.resultsText);
         homeButton = (Button)view.findViewById(R.id.homeButton);
 
         homeButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                beginSheetResponse();
                 callBack.finishTest();
             }
         });
@@ -78,5 +81,16 @@ public class SwayScoreFragment extends Fragment {
         }
         return resultStr.toString();
     }
+
+    private void beginSheetResponse(){
+        SharedPreferences prefs = this.getActivity().getSharedPreferences("PrefsFile", Context.MODE_PRIVATE);
+        int userID = prefs.getInt("user",0);
+        if (userID == 0) {
+            Log.d("Tag","Missing userID!");
+        }
+        ((SwayTest)getActivity()).sendToClassSheet("t8p0" + userID,(float)getAvg());
+        ((SwayTest)getActivity()).sendToGroupSheet("t8p0" + userID,results);
+    }
+
 
 }
--- app/src/main/java/com/example/bethanywong/msapp/SwayTest.java
+++ app/src/main/java/com/example/bethanywong/msapp/SwayTest.java
@@ -1,20 +1,30 @@
 package com.example.bethanywong.msapp;
 
+import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.os.Bundle;
+import android.util.Log;
+
+import edu.umd.cmsc436.sheets.Sheets;
 
 import static com.example.bethanywong.msapp.SwayTestFragment.newInstance;
 import static com.example.bethanywong.msapp.SwayScoreFragment.newInstance;
 
-public class SwayTest extends FragmentActivity implements SwayTestInstructionFragment.StartSwayTestListener, SwayTestFragment.OnSwayRoundFinishListener, SwayScoreFragment.OnFinishSwayTestListener {
+public class SwayTest extends FragmentActivity
+        implements SwayTestInstructionFragment.StartSwayTestListener,
+        SwayTestFragment.OnSwayRoundFinishListener,
+        SwayScoreFragment.OnFinishSwayTestListener,
+        Sheets.Host {
     public static int NUMBER_OF_TRIALS = 3;
     private FragmentManager fragmentManager;
     private FragmentTransaction transaction;
     private int roundNumber;
     private boolean hasBeenResumed;
-    private double[] results;
+    private float[] results;
+    private Sheets sheet;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,7 +35,10 @@ public class SwayTest extends FragmentActivity implements SwayTestInstructionFra
         transaction = fragmentManager.beginTransaction();
         roundNumber = -1;
         hasBeenResumed = false;
-        results = new double[NUMBER_OF_TRIALS];
+        results = new float[NUMBER_OF_TRIALS];
+        String classID = "1YvI3CjS4ZlZQDYi5PaiA7WGGcoCsZfLoSFM0IdvdbDU";
+        String trialID = "15e8fzzCQcYV3WxwV79g_CSyg-yeTyCrA1Z2e0uwpAiw";
+        sheet = new Sheets(this, getString(R.string.app_name), classID, trialID);
         SwayTestInstructionFragment fragment = new SwayTestInstructionFragment();
 
         // place instructions in view automatically
@@ -62,7 +75,7 @@ public class SwayTest extends FragmentActivity implements SwayTestInstructionFra
     }
 
     public void goToNext(double trialResult) {
-        results[roundNumber] = trialResult;
+        results[roundNumber] = (float)trialResult;
         if (roundNumber < NUMBER_OF_TRIALS-1) {
             startTest();
         } else {
@@ -78,4 +91,45 @@ public class SwayTest extends FragmentActivity implements SwayTestInstructionFra
         finish();
     }
 
+    @Override
+     public int getRequestCode(Sheets.Action action) {
+        switch (action) {
+            case REQUEST_ACCOUNT_NAME:
+                return 1;
+            case REQUEST_AUTHORIZATION:
+                return 2;
+            case REQUEST_PERMISSIONS:
+                return 3;
+            case REQUEST_PLAY_SERVICES:
+                return 4;
+            default:
+                return -1;
+        }
+    }
+    @Override
+    public void notifyFinished(Exception e) {
+        if (e != null) {
+            throw new RuntimeException(e);
+        }
+        Log.i(getClass().getSimpleName(), "Done");
+    }
+
+    @Override
+    public void onRequestPermissionsResult (int requestCode, @NonNull String permissions[], @NonNull int[] grantResults) {
+        this.sheet.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        this.sheet.onActivityResult(requestCode, resultCode, data);
+    }
+
+    public void sendToGroupSheet(String userId, float[] trials){
+        sheet.writeTrials(Sheets.TestType.HEAD_SWAY,userId,trials);
+    }
+
+    public void sendToClassSheet(String userId, float avg){
+        sheet.writeData(Sheets.TestType.HEAD_SWAY, userId, avg);
+    }
 }
