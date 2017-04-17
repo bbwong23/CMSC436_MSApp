8a941b1bb4b259ad5fb248ef3a7f72a372e99d5a
bbwong23 <bethanywong1216@gmail.com>
2017-04-17 18:30:42 -0400

    added scoring and trials

--- app/src/main/java/com/example/bethanywong/msapp/StepTest.java
+++ app/src/main/java/com/example/bethanywong/msapp/StepTest.java
@@ -5,8 +5,11 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
+import android.media.AudioManager;
+import android.media.ToneGenerator;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.os.SystemClock;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
@@ -16,12 +19,16 @@ import android.widget.Toast;
 public class StepTest extends AppCompatActivity {
     private Sensor sensor;
     private SensorManager mSensorManager;
-    private int distance;
+    private float[] distance;
     private Button start;
     private boolean testStarted = false;
     private CountDownTimer timer;
     private TextView instruction;
-    private CountDownTimer testTimer;
+    private int steps = 0;
+    private float[] duration;
+    private int trialNum = 0;
+    private float[] scores;
+    ToneGenerator toneAlarm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,6 +44,8 @@ public class StepTest extends AppCompatActivity {
             Toast.makeText(this, "Step Sensor not found", Toast.LENGTH_SHORT).show();
             finish();
         }
+
+        toneAlarm = new ToneGenerator(AudioManager.STREAM_ALARM, 100);
 
         start = (Button) findViewById(R.id.button);
         start.setOnClickListener(new View.OnClickListener() {
@@ -57,20 +66,15 @@ public class StepTest extends AppCompatActivity {
 
         @Override
         public void onSensorChanged(SensorEvent event) {
-            if (testStarted)
-                distance+=2;
+            if (testStarted) {
+                distance[trialNum] += 2;
+                steps++;
+            }
         }
     };
 
     private void startTest() {
-        testTimer = new CountDownTimer(10000, 1000) {
-            public void onTick(long millisUntilFinished) {
-            }
-
-            public void onFinish() {
-                finishTest();
-            }
-        };
+        final float startTime = System.currentTimeMillis();
 
         timer = new CountDownTimer(3000, 1000) {
             public void onTick(long millisUntilFinished) {
@@ -78,19 +82,38 @@ public class StepTest extends AppCompatActivity {
             }
 
             public void onFinish() {
-                instruction.setText("Walk!");
+                instruction.setText("Round " + trialNum+1 + "\n\nWalk!");
                 testStarted = true;
-                testTimer.start();
+                while (steps != 25) {
+                    // let the loop run till it hits 25
+                }
+                // alarm goes off when 25 steps are completed, meaning trial is over
+                toneAlarm.startTone(ToneGenerator.TONE_CDMA_ALERT_CALL_GUARD, 1000);
+
+                // Compute/display scores
+                duration[trialNum] = System.currentTimeMillis() - startTime;
+                finishTest();
             }
         };
+        start.setEnabled(false);
+        timer.start();
     }
 
     private void finishTest() {
-        instruction.setText("Press the start button and start walking!");
-        testStarted = false;
+        if (trialNum < 2) {
+            // score = inches/second traveled in 25 steps
+            scores[trialNum] = distance[trialNum] / (duration[trialNum] / 1000);
 
-        //calculate distance / second
-
-        distance = 0;
+            // reset values
+            start.setEnabled(true);
+            testStarted = false;
+            steps = 0;
+            trialNum++;
+            instruction.setText("Round " + trialNum+1 + "\n\nPress the start button and start walking!");
+        } else {
+            float finalScore = (scores[0] + scores[1] + scores[2])/3;
+            instruction.setText("Final Score: " + finalScore + "\nTrial Scores:" +
+                    "\n" + scores[0] + "\n" + scores[1] + "\n" + scores[2]);
+        }
     }
 }
